d2db40f47c569dfdafb501d703aa4f94aca3597d
Husni and nijin <93671844+nijin-husni@users.noreply.github.com>
2021-12-26 22:16:25 +0530

    ꪶ͢💝ᴍᴀᴀʟᴜᴛᴛʏ_ᴠ3💝ꫂ⁩

--- bot/translation.py
+++ bot/translation.py
@@ -71,7 +71,6 @@ For More Details Click Help Button 💞..
 ╚✘
 </b>
 """
-"""
 
     OWNER_TEXT = """<b>HI MWONUSE</b>
 </b>╭───𒆜𝙊𝙒𝙉𝙀𝙍𝙎𒆜
@@ -80,4 +79,376 @@ For More Details Click Help Button 💞..
 │🥂 <b><i><a href="https://t.me/nijinhusni">ꪶ͢ʜᴜsɴɪꫂ</a></i></b>
 ╰───────────────𒈒
 </b>
+"""    
+
+    OWNER_TEXT = """<b>◄━⦁💝 MAALUTTY_💝⦁━► </b>
+🔮 <b>.xmedia</b>
+💘  It is a plugin with more than 25 media tools.
+
+🔮 <b>.ownercmnd</b>
+💘  set of commands for bot user or sudo
+
+🔮 <b>.codtts</b>
+💘  language code to change the voice in .tts & also for .trt translation
+
+🔮 <b>.adan</b>
+💘  Finds prayer time. </b>⌨️ Eg:- :</b> .prayer <city>
+
+🔮 <b>.rename</b>
+💘  [object Object]
+
+🔮 <b>.anime</b>
+💘  random anime image 
+
+🔮 <b>.maaluforward</b>
+💘  Forwards replied audio as voice note with verified info
+
+🔮 <b>.apkmod</b> 
+
+🔮 <b>.fatp</b> 
+
+🔮 <b>.ask</b>
+
+🔮 <b>.infoask
+
+🔮 <b>.love
+
+🔮 <b>.infolove</b> 
+
+🔮 <b>.ttp</b>
+💘  Converts text to plain painting.
+
+🔮 <b>.attp</b>
+💘  Adds rainbow effect to the text as a sticker.
+
+🔮 <b>.emoji
+
+🔮 <b>.pmaudio</b> 
+💘  Forwards replied audio as voice note with verified info as broadcast to all participants
+
+🔮 <b>.nijin</b>
+💘  .
+
+🔮 <b>.husni</b>
+💘  .
+
+🔮 <b>.bgm</b>
+💘  turn on and turn off bgm. -bot owner command </b>⌨️ Eg:- :</b> .bgm on / off 
+
+🔮 <b>.sendi</b>
+💘  Download status from wa
+
+🔮 <b>.sendv</b>
+💘  Download status from wa
+
+🔮 <b>.carbon</b>
+💘  It sends carbon picture
+
+🔮 <b>.bgm</b>
+💘  change reply message BGM mode </b>⌨️ Eg:- :</b> .bgm one / two 
+
+🔮 <b>.mp3</b>
+💘  maalutty Converts video to audio.
+
+🔮 <b>.photo</b>
+💘  Converts the sticker to a photo.
+
+🔮 <b>.mp4</b>
+💘  Converts animated stickers to video.
+
+🔮 <b>.doc</b>
+💘  CONVERT TO DOCUMENT AND ADD GIVEN NAME </b>⌨️ Eg:- :</b> .doc Maalutty </b>replace Maalutty with desired name</b> 
+
+🔮 <b>.dict</b>
+💘  Use it as a dictionary.Eg: .dict en_US;lead For supporting languages send <b>.lngcode</b>
+
+🔮 <b>.roll</b>
+💘  Roll dice randomly.
+
+🔮 <b>.emo</b>
+💘  You Can Png From Any Emoji
+
+🔮 <b>.png
+
+🔮 <b>.git</b>
+💘  It sends high resolution wallpapers.
+
+🔮 <b>.welcome</b>
+💘  It sets the welcome message. If you leave it blank it shows the welcome message.
+
+🔮 <b>.goodbye</b>
+💘  Sets the goodbye message. If you leave blank it shows the goodbye message.
+
+🔮 <b>.print</b>
+💘  Prints the inside of the file on the server.
+
+🔮 <b>.bashmedia</b>
+💘  Sends audio video and photos inside the server. </b>⌨️ Eg:- :</b> video.mp4 && media/gif/pic.mp4 
+
+🔮 <b>.addserver</b>
+💘  Uploads image audio or video to the server.
+
+🔮 <b>.term1</b>
+💘  Allows to run the command on the servers shell.
+
+🔮 <b>.findvid</b>
+💘  Shows the technical information of the replied video.
+
+🔮 <b>.pm</b>
+💘  Sends a private message to the replied person.
+
+🔮 <b>.pmsend</b>
+💘  Sends a private voice message to the respondent.
+
+🔮 <b>.antilink</b>
+💘  Activates the Antilink tool. </b>⌨️ Eg:- :</b> .antilink on / off 
+
+🔮 <b>.menu</b>
+💘  it send bot menu
+
+🔮 <b>.maaluforward</b>
+💘  its foraward replied audio
+
+🔮 <b>.moretxt</b>
+💘  more txtit commands
+
+🔮 <b>.ffire</b>
+💘  add your text to random freefire logo
+
+🔮 <b>.emo</b>
+💘  emogi to png
+
+🔮 <b>.news
+
+🔮 <b>.notes</b>
+💘  Shows all your existing notes.
+
+🔮 <b>.save</b>
+💘  Reply a message and type .save or just use .save <Your note> without replying
+
+🔮 <b>.deleteNotes</b>
+💘  Deletes </b>all</b> your saved notes.
+
+🔮 <b>.owner</b>
+💘  shows the detail of bot owner
+
+🔮 <b>.pdf</b>
+💘  Get screenshot of links in pdf
+
+🔮 <b>.presence</b>
+💘  You can set Bot Presence. -owner command </b>⌨️ Eg:- :</b> .presence online / offline / typing / recording 
+
+🔮 <b>.jid</b>
+💘  Giving users JID.
+
+🔮 <b>.random</b>
+💘  word image
+
+🔮 <b>.rashmika</b>
+💘  random rashmika images 
+
+🔮 <b>.brdmore</b>
+💘  add readmore before your text
+
+🔮 <b>.rdmore</b>
+💘  add readmore before your text
+
+🔮 <b>.removebg</b>
+💘  Removes the background of the photos.
+
+🔮 <b>.warn</b>
+💘  Sends reports to group admins.
+
+🔮 <b>.scan</b>
+💘  Checks whether the entered number is registered on WhatApp.
+
+🔮 <b>.trt</b>
+💘  It translates with Google Translate. You must reply any message. </b>⌨️ Eg:- :</b> .trt tr it (From Turkish to Italian) 
+
+🔮 <b>.detectlang</b>
+💘  Guess the language of the replied message.
+
+🔮 <b>.currency
+
+🔮 <b>.tts</b>
+💘  It converts text to sound.
+
+🔮 <b>.song</b>
+💘  Uploads the song you wrote.
+
+🔮 <b>.video</b>
+💘  Downloads video from YouTube.
+
+🔮 <b>.sing</b>
+💘  It sings song that you have written
+
+🔮 <b>.song</b>
+💘  Uploads the song you wrote for ios users.
+
+🔮 <b>.wiki</b>
+💘  Searches query on Wikipedia.
+
+🔮 <b>.img</b>
+💘  Searches for related pics on Google.
+
+🔮 <b>.github</b>
+💘  Collects github information from the given username.⌨️ Example: .github phaticusthiccy
+
+🔮 <b>.lyric</b>
+💘  Finds the lyrics of the song.
+
+🔮 <b>.covid</b>
+💘  Shows the daily and overall covid table of more than 15 countries.
+
+🔮 <b>.sweather</b>
+💘  Gives you the weekly interpretations of space weather observations provided by the Space Weather Research Center (SWRC) for a p.
+
+🔮 <b>.compliment</b>
+💘  It sends complimentry sentenses
+
+🔮 <b>.hmod</b>
+💘  Finds mod apps from happymod
+
+🔮 <b>.insult</b>
+💘  It sends insulted words
+
+🔮 <b>.movie</b>
+💘  Shows movie info.
+
+🔮 <b>.joke</b>
+💘  It sends random jokes
+
+🔮 <b>.vava</b>
+💘  Chat with Maalutty. Use .maalu <message>
+
+🔮 <b>.owner</b>
+💘  shows the detail of bot owner
+
+🔮 <b>.quote</b>
+💘  It shares famous quotes
+
+🔮 <b>.ss</b>
+💘  Takes a screenshot from the page in the given link.
+
+🔮 <b>.setvar</b>
+💘  It sends high resolution wallpapers.
+
+🔮 <b>.show</b>
+💘  Get info related to tv series and shows
+
+🔮 <b>.ig</b>
+💘  Fetches user informations from instagram
+
+🔮 <b>.animesay</b>
+💘  It writes the text inside the banner the anime girl is holding
+
+🔮 <b>.changesay</b>
+💘  Turns the text into the change my mind poster.
+
+🔮 <b>.trumpsay</b>
+💘  Converts the text to Trumps tweet.
+
+🔮 <b>.bgm</b>
+💘  turn on and turn off bgm. -bot owner command </b>⌨️ Eg:- :</b> .bgm on / off 
+
+🔮 <b>.autosticker</b>
+💘  turn on and turn off bgm. -bot owner command </b>⌨️ Eg:- :</b> .sticker on / off 
+
+🔮 <b>.sudo</b>
+💘  changes sudo numbers </b>⌨️ Eg:- :</b> .sudo </b>your number</b> 
+
+🔮 <b>.caption</b>
+💘  changes all captions </b>⌨️ Eg:- :</b> .caption </b>Made by Maalutty</b> 
+
+🔮 <b>.number</b>
+💘  change user number </b>⌨️ Eg:- :</b> .number </b>Made by Maalutty</b> 
+
+🔮 <b>.deployer</b>
+💘  change user name </b>⌨️ Eg:- :</b> .deployer </b>Made by Maalutty</b> 
+
+🔮 <b>.handlers</b>
+💘  changes handlers </b>⌨️ Eg:- :</b> .handler ^[.!]  
+
+🔮 <b>.botname</b>
+💘  change your bot name </b>⌨️ Eg:- :</b> .botname </b>name</b>  
+
+🔮 <b>.theri </b>
+💘  change your theri commands </b>⌨️ Eg:- :</b> .theri commandcommand 
+
+🔮 <b>.sticker</b>
+💘  It converts your replied photo or video to sticker.
+
+🔮 <b>.ffpack</b>
+💘  Free Fire logo maker
+
+🔮 <b>.ffpack</b>
+💘  Free Fire logo maker
+
+🔮 <b>.alive</b>
+💘  Does bot work?
+
+🔮 <b>.sysd</b>
+💘  Shows the system properties.
+
+🔮 <b>.tagadmin</b>
+💘  Tags group admins.
+
+🔮 <b>.bc</b>
+💘  Sends the replied message to all members in the group.
+
+🔮 <b>.txtit</b>
+💘  Shows text to image tools with unlimited access.
+
+🔮 <b>.antibadword</b>
+💘  turn on & off anti-badword To remove members when they use bad words </b>⌨️ Eg:- :</b> .antibadword on / off 
+
+🔮 <b>.a</b>
+💘  Converts audio to sound recording.
+
+🔮 <b>.unvoice</b>
+💘  Converts audio to sound recording.
+
+🔮 <b>.update</b>
+💘  Checks the update.
+
+🔮 <b>.update now</b>
+💘  It makes updates.
+
+🔮 <b>.url
+
+🔮 <b>.wallpaper</b>
+💘  It sends high resolution wallpapers.
+
+🔮 <b>.wame</b>
+💘  Get a link to the user chat.
+
+🔮 <b>.weather</b>
+💘  Shows the weather.
+
+🔮 <b>.speedtest</b>
+💘  Measures Download and Upload speed.
+
+🔮 <b>.ping</b>
+💘  Measures your ping.
+
+🔮 <b>.short</b>
+💘  Shorten the long link.
+
+🔮 <b>.calc</b>
+💘  Performs simple math operations.
+
+🔮 <b>.whois</b>
+💘  Displays metadata data of group or person.
+
+🔮 <b>.gdmrng</b>
+💘  it sends good morning message
+
+🔮 <b>.gdni8</b>
+💘  it sends good night message
+
+🔮 <b>.xxxtentacion</b>
+💘  random xxxtentacion images 
+
+🔮 <b>.ytv</b>
+💘 ownloads youtube video faster along with views channel name and other info
 """    
